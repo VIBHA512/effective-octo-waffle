53a632f71550d0e993d89b97927f6e3c37601ffb
VIBHA <artvibha2007@gmail.com>
2026-02-28 19:31:57 +0530

    Create 19table.cpp

---
+++ LOOPS/19table.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "TABLE OF 19" << endl;
+
+    for (int i = 1; i <= 10; i++) {
+        cout << i << " * 19 = " << i * 19 << endl;
+    }
+
+    return 0;
+}
+
