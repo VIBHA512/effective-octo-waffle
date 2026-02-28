8750ca4739b1eacfd3c772b77ee29de34ae9368e
VIBHA <artvibha2007@gmail.com>
2026-02-28 22:31:17 +0530

    Create Floyd's_Triangle.cpp

---
+++ PATTERN/Floyd's_Triangle.cpp
@@ -0,0 +1,23 @@
+/*FLOYD'S TRIANGLE
+enter n 5
+1 
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter n ";
+    cin>>n;
+    int a=1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<a<<" ";
+            a++;
+        }cout<<endl;
+    }
+}
