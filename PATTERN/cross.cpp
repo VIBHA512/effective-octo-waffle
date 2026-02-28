49fa6527f4989538f5e351e965e13f742785e7d8
VIBHA <artvibha2007@gmail.com>
2026-02-28 23:26:47 +0530

    Create cross.cpp

---
+++ PATTERN/cross.cpp
@@ -0,0 +1,26 @@
+/* 
+
+cross star
+enter n 5
+*       * 
+  *   *
+    *
+  *   *
+*       *
+
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter n ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(i==j||i+j==(n+1))
+            cout<<"* ";
+            else
+            cout<<"  ";
+        }cout<<endl;
+    }
+}
